e5e134146563ae971d9668f2d7b848543369611f
SeokHwan13 <seokhwan1113@naver.com>
2025-08-05 21:10:07 +0900

    #6 [feat] 앱 종료 기능 추가

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
+    Scanner sc = new Scanner(System.in);
     public void run() {
-        System.out.println("Hello World");
+        System.out.println("== 게시판 앱 ==");
+        while(true){
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
+            if(cmd.equals("exit")){
+                System.out.print("종료 합니다...");
+                break;
+            }
+        }
     }
 }
